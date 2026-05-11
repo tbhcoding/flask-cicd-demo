dbac5707e66b1f2238f8e5d1b2e9e2c58bca2722
tbhcoding <blbongalos@my.cspc.edu.ph>
2026-05-12 00:03:18 +0800

    Fix failing test

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,4 +10,4 @@ def test_home_page():
     response = client.get("/")
 
     assert response.status_code == 200
-    assert b"This should fail" in response.data
+    assert b"Hello" in response.data
