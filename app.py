82835853e6ba4e7fba69b69b0fad2d8c1d580cff
tbhcoding <blbongalos@my.cspc.edu.ph>
2026-05-12 02:49:48 +0800

    Update homepage message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask CI/CD!"
+    return "Hello from Flask CI/CD! Deployed automatically to azure"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
